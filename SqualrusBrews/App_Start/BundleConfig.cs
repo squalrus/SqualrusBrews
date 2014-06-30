c1d6ad5dce205bcf27fc29058ec598029146daac
Chad Schulz <chad.awesome@gmail.com>
2014-06-30 00:54:39 -0700

    Use garment

--- SqualrusBrews/App_Start/BundleConfig.cs
+++ SqualrusBrews/App_Start/BundleConfig.cs
@@ -8,30 +8,19 @@ namespace SqualrusBrews
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/respond.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Styles/Reset.css",
+            bundles.Add(new StyleBundle("~/bundles/styles").Include(
                       "~/Styles/Main.css",
-                      "~/Styles/Layout.css",
-                      "~/Styles/Navigation.css",
                       "~/Styles/Sections.css",
-                      "~/Styles/Buttons.css",
-                      "~/Styles/Icons.css",
-                      "~/Styles/Table.css"));
+                      "~/Styles/BrewList.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
